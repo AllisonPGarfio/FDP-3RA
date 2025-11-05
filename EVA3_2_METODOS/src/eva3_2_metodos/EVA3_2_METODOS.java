46ddadf5fcd1604ff0ea0712d175422b418fc100
AllisonPGarfio <allisonpalomag@gmail.com>
2025-11-05 17:53:21 -0600

    Add files via upload

---
+++ EVA3_2_METODOS/src/eva3_2_metodos/EVA3_2_METODOS.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_2_metodos;
+
+/**
+ *
+ * @author garfi
+ */
+public class EVA3_2_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        repetirMensaje("HOLA MUNDO",5);
+    }
+        public static void repetirMensaje(String mensaje, int repeticiones){//ENCABEZADO DE METODO 
+        for(int i=1; i<=repeticiones; i++){
+            System.out.println(mensaje);
+        }
+        
+      }
+    }
+    
+
